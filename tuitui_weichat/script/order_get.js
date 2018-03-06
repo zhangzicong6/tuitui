4ef4972db809aeed5435b3094583fc0353168da2
root <lionel@buzzy.cc>
2018-03-06 15:32:38 +0800

    订单匹配

--- tuitui_weichat/script/order_get.js
+++ tuitui_weichat/script/order_get.js
@@ -9,61 +9,97 @@ var weichat_conf = require('../conf/weichat.json');
 var weichat_apis ={};
 var async = require('async');
 
+function next_up(_id){
+		if(_id){
+			return update_order(_id,next_up);
+		}else{
+			console.log('end');
+			return;
+		}
+}
+
 function get_order(){
-	update_order(null);
+	update_order(null,next_up);
 }
 
 
-function update_order(_id){
-	UserOrderModel.fetch(_id,function(err,result){
+function update_order(_id,next){
+	UserOrderModel.fetch(_id,function(err,user_orders){
+		console.log('user_orders');
+		async.each(user_orders,
+			function(order,cb){
+				TaobaoOrderModel.findOne({order_id:order.order_number},function(error,taobao){
+					if(!taobao){
+						return cb(null,null);
+					}
+					async.waterfall([
+							function(callback){
+								if(order.status == 0 ){
+									order.create_at = taobao.create_at;
+									UserModel.findOne({openid:order.openid},function(err,user){
+										if(!user){
 
-		async.each(result,function(order,cb){
-			TaobaoOrderModel.findOne({order_id:order.order_number},function(error,taobao){
-				if(taobao == null){
-					return cb(null);
-				}
-				if(order.status == 0 ){
-					order.create_at = taobao.create_at;
-					UserModel.findOne({openid:order.openid},function(err,user){
-						if(!weichat_apis[user.code]){
-							var config = weichat_conf[user.code];
-							weichat_apis[user.code] = new WechatAPI(config.appid, config.appsecret);
-						}
-						user.all_count += 1;
-						user.unfinished_count +=1;
-						user.save();
-						var client = weichat_apis[user.code];
-						var str = '恭喜您！订单【'+taobao.order_id+'】【'+taobao.goods_info+'】跟踪成功！\r\n'+
-									'[须知]:商品确认收货后半小时返利会添加到个人账户\r\n\r\n一一一🍉常用指令一一一\r\n'+
-									'账户信息请回复：个人信息\r\n订单查询请回复：订单\r\n余额提现请回复：提现';
-						client.sendText(msg.openid, str, function(err,result){
-							console.log(err);
-						});
+										}else{
+											if(!weichat_apis[user.code]){
+												var config = weichat_conf[user.code];
+												weichat_apis[user.code] = new WechatAPI(config.appid, config.appsecret);
+											}
+											user.all_count += 1;
+											user.unfinished_count +=1;
+											user.save(function(){
+
+											});
+											var client = weichat_apis[user.code];
+											var str = '恭喜您！订单【'+taobao.order_id+'】【'+taobao.goods_info+'】跟踪成功！\r\n'+
+														'[须知]:商品确认收货后半小时返利会添加到个人账户\r\n\r\n一一一🍉常用指令一一一\r\n'+
+														'账户信息请回复：个人信息\r\n订单查询请回复：订单\r\n余额提现请回复：提现';
+											/*client.sendText(msg.openid, str, function(err,result){
+												console.log(err);
+											});*/
+											
+											callback(null);
+										}
+									});
+								}else{
+									callback(null);
+								}
+							},
+							function(callback){
+								
+								order.status = getOrderStatus(taobao.order_status);
+								if(order.status == -1){
+									UserModel.findOneAndUpdate({openid:order.openid},{$inc:{unfinished_count:-1,finished_count:1}},function(error,u){
+										//console.log(error);
+									});
+								}
+								if( order.status == 3){
+									var add_cash = parseFloat(taobao.order_tkCommFee);
+									order.tk_comm_fee = add_cash;
+									AddFreeOrderModel.create({openid:order.openid,type:1,cash:add_cash,order_number:order.order_number});
+									UserModel.findOneAndUpdate({openid:order.openid},{$inc:{current_balance:add_cash,unfinished_count:-1,finished_count:1}},function(error,u){
+										//console.log(error);
+									});
+								}
+								order.save();
+								callback(null);
+							}
+						],function(error,result){
+							
+							return cb(null,null);
 					});
+					
+				});
+			},
+			function(error,result){
+				console.log('next');
+				if(user_orders.length==50){
+					return next(user_orders[49]._id);
+				}else{
+					return next(null);
 				}
-				order.status = getOrderStatus(taobao.order_status);
-				if(order.status == -1){
-					UserModel.findOneAndUpdate({openid:order.openid},{$inc:{unfinished_count:-1,finished_count:1}},function(error,user){
-						console.log(error);
-					});
-				}
-				if( order.status == 3){
-					var add_cash = parseFloat(taobao.order_tkCommFee);
-					order.tk_comm_fee = add_cash;
-					AddFreeOrderModel.create({openid:order.openid,type:1,cash:add_cash,order_number:order.order_number});
-					UserModel.findOneAndUpdate({openid:order.openid},{$inc:{current_balance:add_cash,unfinished_count:-1,finished_count:1}},function(error,user){
-						console.log(error);
-					});
-				}
-				order.save();
-				cb(null);
-			});
+				
 		});
-		if(result.length==50){
-			update_order(result[49]._id);
-		}
-	},function(error,result){
-		console.log(error);
+
 	});
 }
 
@@ -78,6 +114,7 @@ function getOrderStatus(status){
 		return 3;
 	}
 }
+
 
 var rule    = new schedule.RecurrenceRule();  
 var times   = [1,6,11,16,21,26,31,36,41,46,51,56];  
