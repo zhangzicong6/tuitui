70c7abc7ee5fed72697b107bdbe0c47b5064b6a8
root <lionel@buzzy.cc>
2018-03-07 14:21:43 +0800

    配置菜单

--- tuitui_weichat/routes/users.js
+++ tuitui_weichat/routes/users.js
@@ -5,6 +5,11 @@ var UserModel = require('../model/User.js');
 var UserOrderModel = require('../model/UserOrder.js');
 var TaobaoOrderModel = require('../model/TaobaoOrder.js');
 
+var WechatAPI = require('wechat-api');
+var weichat_conf = require('../conf/weichat.json');
+var menus = require('../conf/menu.json');
+
+
 
 router.use('/data', function(req, res, next) {
 	console.log('上传订单');
@@ -23,11 +28,34 @@ router.use('/data', function(req, res, next) {
 
 router.use('/reset', function(req, res, next) {
 	console.log('重置订单');
-	var openid = req.query.openid
+	var openid = req.query.openid;
 	UserModel.findOneAndUpdate({openid:openid},{finished_count:0,unfinished_count:0,all_count:0},function(err,res){
 		console.log(res);
 	});
 	res.send('success');
 });
+
+router.use('create_menu',function(req, res, next){
+	var code = req.query.code;
+	if(code){
+		createMenu(code);
+		res.send('success');
+	}else{
+		res.send('need params code');
+	}
+});
+
+function createMenu(code) {
+	var config = weichat_conf[code];
+	var api = new WechatAPI(config.appid, config.appsecret);
+	var menu = menus[config];
+	api.removeMenu(function(err,res){
+		api.createMenu(menu, function(err,res){
+			console.log(res);
+		});
+	});
+	
+}
+
 
 module.exports = router;
