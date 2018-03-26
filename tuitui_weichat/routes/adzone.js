7b62c519764fb82fdf767e5f18167552692219f3
root <lionel@buzzy.cc>
2018-03-26 18:59:37 +0800

    可配置

--- tuitui_weichat/routes/adzone.js
+++ tuitui_weichat/routes/adzone.js
@@ -3,7 +3,8 @@ var router = express.Router();
 
 
 router.get('/getjs/:time.js', function(req, res, next) {
-  res.render('adzone/mew_js', { code: '￥Tbzf0Kk0SQZ￥',text:'【现货】土耳其恶魔之眼-小麋人原创手工S925纯银耳环（不过敏），精致复古，耐看百搭~￥26.8元 【包邮】' });
+	var conf = require('../conf/adzone.json');
+    res.render('adzone/mew_js', conf);
 });
 
 module.exports = router;
