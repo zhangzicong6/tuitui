0f5ac5f20f5b4ae5116beb5d04f8c05a84c71c76
root <lionel@buzzy.cc>
2018-04-28 15:29:22 +0800

    上传教程

--- tuitui_weichat/routes/weichat.js
+++ tuitui_weichat/routes/weichat.js
@@ -38,7 +38,7 @@ router.use('/:code', function(request, response, next_fun) {
 				if (message.MsgType === 'text') {
 				    var text = message.Content.trim();
 				 	if(text === '帮助'){
-				 		res.reply('图文教程:http://t.cn/RETghsf\r\n———— 省钱攻略 ———— \r\n1.打开手机淘宝，选中购买的产品。\r\n'+
+				 		res.reply('文字教程：http://t.cn/Rlz6JkV\r\n视频教程：http://t.cn/RK37GMb\r\n\r\n———— 省钱攻略 ———— \r\n1.打开手机淘宝，选中购买的产品。\r\n'+
 				 			'2.点击商品名右侧的“分享(有赏)”，分享给我。\r\n3.复制我返回的信息。\r\n4.打开淘宝放入购物车或付款购买。\r\n注:不可使用淘金币进行抵扣\r\n'+
 				 			'5.点击查看订单，把订单号发给我获得返利。\r\n———— 常用指令———— \r\n账户信息请回复:个人信息\r\n订单查询请回复:订单\r\n余额提现请回复:提现 \r\n详细教程请回复:帮助');
 				 	}else if(text === '订单'){
@@ -79,7 +79,7 @@ router.use('/:code', function(request, response, next_fun) {
 							if(config.sub_replay == 0){
 								res.reply('');
 							}else{
-								res.reply('省钱助手欢迎您！\r\n回复10000或好友邀请码领红包!\r\n一一一一🍒使用攻略一一一一\r\n<搜索优惠>回复：搜索+商品名称\r\n<指定商品优惠查询>请将淘宝商品分享给我！\r\n文字教程：http://t.cn/Rn1uw6o\r\n账户信息请回复：个人信息\r\n订单查询请回复：订单\r\n余额提现请回复：提现\r\n详细教程请回复：帮助\r\n')
+								res.reply('省钱助手欢迎您！\r\n回复10000或好友邀请码领红包!\r\n一一一一🍒使用攻略一一一一\r\n<搜索优惠>回复：搜索+商品名称\r\n<指定商品优惠查询>请将淘宝商品分享给我！\r\n文字教程：http://t.cn/Rlz6JkV\r\n视频教程：http://t.cn/RK37GMb\r\n账户信息请回复：个人信息\r\n订单查询请回复：订单\r\n余额提现请回复：提现\r\n详细教程请回复：帮助\r\n')
 							}
 						}else{
 							var book_id = book_wechat_conf.book_wechat_map[request.params.code];
