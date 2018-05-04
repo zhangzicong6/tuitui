43df3569e159df6fb5762fd01a657622a65b6a85
root <lionel@buzzy.cc>
2018-05-04 13:05:09 +0800

    Ok

---
+++ tuitui_weichat/model/UserWaitMessage.js
@@ -0,0 +1,42 @@
+var mongoose = require('mongoose');
+var Schema = mongoose.Schema;
+var connect_url = require('../conf/proj.json').mongodb;
+var db = mongoose.createConnection(connect_url); 
+
+var UserWaitMessageSchema = new Schema({
+	openid:String,
+	code:String,
+    status:Number,
+    user_status:Number,
+    action_time:Number,
+	createAt: {
+      type: Date,
+      default: Date.now
+	},
+	updateAt: {
+	  type: Date,
+	  default: Date.now
+	}
+},{
+	timestamps: { createdAt: 'createAt', updatedAt: 'updateAt' }
+});
+
+UserWaitMessageSchema.statics = {
+    fetch(id, cb) {
+        if (id) {
+            return this.find({_id: {$lt: id}})
+                .limit(50)
+                .sort({'_id':-1})
+                .exec(cb);
+            }else {
+                return this.find({})
+                .limit(50)
+                .sort({'_id':-1})
+                .exec(cb);
+            }
+        
+    }
+}
+
+var UserWaitMessageModel = db.model('UserWaitMessage', UserWaitMessageSchema);
+module.exports = UserWaitMessageModel;
---
+++ tuitui_weichat/script/send_service_message.js
@@ -0,0 +1,18 @@
+var WechatAPI = require('wechat-api');
+var weichat_conf = require('../conf/weichat.json');
+var schedule = require("node-schedule");
+var UserModel = require('../model/User.js');
+var UserWaitMessageModel = require('../model/UserWaitMessage.js');
+var async = require('async');
+var code = '3'
+
+function copy_user(){
+	//从用户表拷贝到信息表
+	
+}
+
+function send_message(){
+	//发送消息
+
+}
+
