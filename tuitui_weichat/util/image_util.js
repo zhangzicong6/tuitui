e7b58f0c42da6652829b3d58dc24a84805df73b8
efdssdf <efdssdf@gmail.com>
2018-05-23 17:39:19 +0800

    'img'

--- tuitui_weichat/util/image_util.js
+++ tuitui_weichat/util/image_util.js
@@ -103,9 +103,9 @@ function user_img(ticket, qr_name, nickname, headimgurl, callback) {
 
 function getUserImg(ticket, nickname, headimgurl, callback) {
     memcached.get('qr_' + ticket, function (err, qr) {
-        // if (qr) {
-        //     return callback(qr);
-        // }
+        if (qr) {
+            return callback(qr);
+        }
         var qr_url = 'https://mp.weixin.qq.com/cgi-bin/showqrcode?ticket=' + ticket;
         console.log(qr_url, '-----------------qr_url');
         var qr_name = Date.now() + '' + parseInt(Math.random() * 10000) + '.jpg';
