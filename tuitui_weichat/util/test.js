09b0c910f20af57aa90b37358319428adf49b384
efdssdf <efdssdf@gmail.com>
2018-05-23 18:05:32 +0800

    'delmem'

--- tuitui_weichat/util/test.js
+++ tuitui_weichat/util/test.js
@@ -1,23 +1,14 @@
-var fs = require('fs')
-    , gm = require('gm');
-var request = require('request');
-var exec = require('child_process').exec;
-process.env.PATH += ":/usr/local/GraphicsMagick-1.3.28/bin";
+var Memcached = require('memcached');
+var memcached = new Memcached('127.0.0.1:11211');
 
 function test() {
-    var resize_cmd = 'gm "convert" "' + __dirname +  '/user_image/15270569225682899.jpg" "-resize" "360x" "' + __dirname + '/user_image/small_15270569225682899.jpg.jpg"';
-    exec(resize_cmd, function (error, stdout, stderr) {
-        if (error) {
-            console.log(error,'------------error1');
-        }
-        var mosaic_cmd = 'gm "convert" "-page" "+0+0" "' + __dirname + '/user_image/tmp_bg.jpg" "-page" "+766+1501" "' + __dirname + '/user_image/small_15270569225682899.jpg" "-page" "+566+1501" "' + __dirname + 'head_15270569225682899.jpg" "-mosaic" "' + __dirname + '/user_image/final.jpg"'
+    memcached.delete('qr_gQF87zwAAAAAAAAAAS5odHRwOi8vd2VpeGluLnFxLmNvbS9xLzAydHhDdklwdXdkYWgxZnAwSWhyY1kAAgTZMwVbAwQAjScA', function (err, qr) {
+    })
+    memcached.delete('user_gQF87zwAAAAAAAAAAS5odHRwOi8vd2VpeGluLnFxLmNvbS9xLzAydHhDdklwdXdkYWgxZnAwSWhyY1kAAgTZMwVbAwQAjScA', function (err, qr) {
+    })
+    memcached.delete(JSON.stringify({openid:'o3qBK0QX8pKSVxlehps3GSL18NXo'}), function (err, qr) {
+    })
 
-        exec(mosaic_cmd, function (error, stdout, stderr) {
-            if (error) {
-                console.log(error,'------------error2');
-            }
-        });
-    });
 }
 test()
 
