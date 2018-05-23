3a12fa2100382005aebcd1b9d26bcc4f99fb994c
efdssdf <efdssdf@gmail.com>
2018-05-23 16:52:04 +0800

    'img'

--- tuitui_weichat/util/image_util.js
+++ tuitui_weichat/util/image_util.js
@@ -85,7 +85,7 @@ function user_img(ticket, qr_name, nickname, headimgurl, callback) {
             }
             var mosaic_cmd = 'gm "convert" "-page" "+0+0" "' + __dirname + '/user_image/tmp_bg.jpg" ' +
                 '"-page" "+766+1501" "' + __dirname + '/user_image/small_' + qr_name + '" "-page" "+336+1501" "' + __dirname + '/user_image/smallhead_'
-                + qr_name + '" "-font"' + __dirname + '/china.TTF" "-fill" "white" "-pointsize" "40" "-draw" "text 336,2001 ' + nickname + '"' +
+                + qr_name + '" "-font" "' + __dirname + '/user_image/china.TTF" "-fill" "white" "-pointsize" "40" "-draw" "text 336,2001 ' + nickname + '"' +
                 ' "-mosaic""' + __dirname + '/user_image/' + qr_name + '"'
 
             exec(mosaic_cmd, function (error, stdout, stderr) {
