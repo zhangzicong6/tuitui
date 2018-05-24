448459d0a36f5e0136e4c68d64601f0f61efe610
efdssdf <efdssdf@gmail.com>
2018-05-24 12:37:06 +0800

    'fix'

--- tuitui_weichat/util/mem.js
+++ tuitui_weichat/util/mem.js
@@ -3,7 +3,9 @@ var memcached = new Memcached('127.0.0.1:11211');
 
 module.exports.getContent = function (ticket) {
     return new Promise((resolve, reject) => {
-        memcached.get(ticket, function (err, content) {
+        console.log(ticket, '-----------ticket1')
+        memcached.get('user_' + ticket, function (err, content) {
+            console.log(content, '-----------------content1')
             resolve(content)
         });
     })
