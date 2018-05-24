9044f561b74f98064a87da1ed680d1b56dd3684c
efdssdf <efdssdf@gmail.com>
2018-05-24 17:14:54 +0800

    'fix'

--- tuitui_weichat/util/test.js
+++ tuitui_weichat/util/test.js
@@ -3,33 +3,33 @@ var memcached = new Memcached('127.0.0.1:11211');
 var UserModel = require('../model/User.js');
 
 function test() {
-    memcached.delete('image_gQF87zwAAAAAAAAAAS5odHRwOi8vd2VpeGluLnFxLmNvbS9xLzAydHhDdklwdXdkYWgxZnAwSWhyY1kAAgTZMwVbAwQAjScA', function (err, qr) {
-    })
-    memcached.delete('user_gQF87zwAAAAAAAAAAS5odHRwOi8vd2VpeGluLnFxLmNvbS9xLzAydHhDdklwdXdkYWgxZnAwSWhyY1kAAgTZMwVbAwQAjScA', function (err, qr) {
-    })
-    memcached.delete(JSON.stringify({openid:'o3qBK0QX8pKSVxlehps3GSL18NXo'}), function (err, qr) {
-    })
-    memcached.delete('image_gQF87zwAAAAAAAAAAS5odHRwOi8vd2VpeGluLnFxLmNvbS9xLzAydHhDdklwdXdkYWgxZnAwSWhyY1kAAgTZMwVbAwQAjScA', function (err, qr) {
-    })
-    memcached.delete('user_gQF87zwAAAAAAAAAAS5odHRwOi8vd2VpeGluLnFxLmNvbS9xLzAydHhDdklwdXdkYWgxZnAwSWhyY1kAAgTZMwVbAwQAjScA', function (err, qr) {
-    })
-    memcached.delete(JSON.stringify({openid:'o3qBK0RXH4BlFLEIksKOJEzx08og'}), function (err, qr) {
-    })
-    memcached.delete('image_gQG_7zwAAAAAAAAAAS5odHRwOi8vd2VpeGluLnFxLmNvbS9xLzAyZlhfZko3dXdkYWgxZXhVSmhyY0MAAgShawZbAwQAjScA', function (err, qr) {
-    })
-    memcached.delete('user_gQG_7zwAAAAAAAAAAS5odHRwOi8vd2VpeGluLnFxLmNvbS9xLzAyZlhfZko3dXdkYWgxZXhVSmhyY0MAAgShawZbAwQAjScA', function (err, qr) {
-    })
-    memcached.delete(JSON.stringify({openid:'o3qBK0ZM6UHi58UK_Yy3YymzdEcc'}), function (err, qr) {
-    })
-    UserModel.deleteOne({openid:'o3qBK0RXH4BlFLEIksKOJEzx08og'},function (err,data) {
-        console.log(data,'-------------------host')
-    })
-    UserModel.deleteOne({openid:'o3qBK0QX8pKSVxlehps3GSL18NXo'},function (err,data) {
-        console.log(data,'-------------------father')
-    })
-    UserModel.deleteOne({openid:'o3qBK0ZM6UHi58UK_Yy3YymzdEcc'},function (err,data) {
-        console.log(data,'-------------------user')
-    })
+    // memcached.delete('image_gQF87zwAAAAAAAAAAS5odHRwOi8vd2VpeGluLnFxLmNvbS9xLzAydHhDdklwdXdkYWgxZnAwSWhyY1kAAgTZMwVbAwQAjScA', function (err, qr) {
+    // })
+    // memcached.delete('user_gQF87zwAAAAAAAAAAS5odHRwOi8vd2VpeGluLnFxLmNvbS9xLzAydHhDdklwdXdkYWgxZnAwSWhyY1kAAgTZMwVbAwQAjScA', function (err, qr) {
+    // })
+    // memcached.delete(JSON.stringify({openid:'o3qBK0QX8pKSVxlehps3GSL18NXo'}), function (err, qr) {
+    // })
+    // memcached.delete('image_gQF87zwAAAAAAAAAAS5odHRwOi8vd2VpeGluLnFxLmNvbS9xLzAydHhDdklwdXdkYWgxZnAwSWhyY1kAAgTZMwVbAwQAjScA', function (err, qr) {
+    // })
+    // memcached.delete('user_gQF87zwAAAAAAAAAAS5odHRwOi8vd2VpeGluLnFxLmNvbS9xLzAydHhDdklwdXdkYWgxZnAwSWhyY1kAAgTZMwVbAwQAjScA', function (err, qr) {
+    // })
+    // memcached.delete(JSON.stringify({openid:'o3qBK0RXH4BlFLEIksKOJEzx08og'}), function (err, qr) {
+    // })
+    // memcached.delete('image_gQG_7zwAAAAAAAAAAS5odHRwOi8vd2VpeGluLnFxLmNvbS9xLzAyZlhfZko3dXdkYWgxZXhVSmhyY0MAAgShawZbAwQAjScA', function (err, qr) {
+    // })
+    // memcached.delete('user_gQG_7zwAAAAAAAAAAS5odHRwOi8vd2VpeGluLnFxLmNvbS9xLzAyZlhfZko3dXdkYWgxZXhVSmhyY0MAAgShawZbAwQAjScA', function (err, qr) {
+    // })
+    // memcached.delete(JSON.stringify({openid:'o3qBK0ZM6UHi58UK_Yy3YymzdEcc'}), function (err, qr) {
+    // })
+    // UserModel.deleteOne({openid:'o3qBK0RXH4BlFLEIksKOJEzx08og'},function (err,data) {
+    //     console.log(data,'-------------------host')
+    // })
+    // UserModel.deleteOne({openid:'o3qBK0QX8pKSVxlehps3GSL18NXo'},function (err,data) {
+    //     console.log(data,'-------------------father')
+    // })
+    // UserModel.deleteOne({openid:'o3qBK0ZM6UHi58UK_Yy3YymzdEcc'},function (err,data) {
+    //     console.log(data,'-------------------user')
+    // })
 }
 test()
 
