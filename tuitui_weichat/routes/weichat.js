b4c522d4c1ae0dcbb73bc0459b147e3c4bd63714
efdssdf <efdssdf@gmail.com>
2018-06-11 17:02:32 +0800

    'fix'

--- tuitui_weichat/routes/weichat.js
+++ tuitui_weichat/routes/weichat.js
@@ -413,7 +413,7 @@ async function bind_user(openid, code, ticket, res) {
     let conf = weichat_conf[code];
     let api = new WechatAPI(conf.appid, conf.appsecret);
     var time = await mem.bindContent(openid)
-    console.log(time,Date.now(),'-----------------------time')
+    console.log(time,Date.now(),Date.now() - time,'-----------------------time')
     if (!time || Date.now() - time > 5 * 1000) {
         let type = await AddFreeOrderModel.findOne({openid: openid, type: 2})
         console.log(type, '---------------type')
