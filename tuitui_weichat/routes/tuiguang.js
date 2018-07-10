1ae3d62f921be44a3ec43a11b0e84b250361390d
unknown <weblixin@163.com>
2018-07-10 16:08:22 +0800

    小说链接功能

--- tuitui_weichat/routes/tuiguang.js
+++ tuitui_weichat/routes/tuiguang.js
@@ -3,7 +3,6 @@ var router = express.Router();
 var TuiGuangModel = require('../model/TuiGuang.js');
 var multer = require('multer');
 var fs = require('fs')
-var mem = require('../util/mem.js')
 
 var upload = multer({
     dest: './public/images/tuiguang'
@@ -17,46 +16,6 @@ router.post('/novel/upload', upload.single('imageFile'), function(req, res, next
         console.log('上传成功!');
     })
     res.send({filename: req.file.filename + '.jpg'});
-})
-
-
-router.get('/novel/:index', function(req, res, next) {
-	var selector = {id: req.params.index}
-    mem.get('tuiguang_'+req.params.index).then(function(value){
-        if(value){
-            console.log('---------get tuiguang value---------')
-            var res_data = JSON.parse(value);
-            res.render('tuiguang/tuiguang',res_data);
-        }else{
-            TuiGuangModel.find(selector, function(err, data){
-                if (err) {
-                    console.log("Error:" + err);
-                }
-                else {
-                    if (data != '') {
-                        var res_data={
-                            title: data[0].title,
-                            headline: data[0].headline,
-                            gonghao: data[0].gonghao,
-                            author: data[0].author,
-                            avator: data[0].avator,
-                            content: data[0].content,
-                            linkUrl: data[0].linkUrl,
-                            statisticsUrl: data[0].statisticsUrl
-                        }
-
-                        mem.set('tuiguang_'+req.params.index,JSON.stringify(res_data),60*1000).then(function(){
-                             console.log('---------set tuiguang value---------')
-                        })
-                        res.render('tuiguang/tuiguang',res_data);
-                    } else {
-                        res.send('没有查询到此链接，请先创建')
-                    }
-                }
-            })
-        }
-    });
-    
 })
 
 router.post('/novel/add', (req, res, next) => {
