f1ce7c422f5c75cdc2e926fe0f5306c1eb65ad86
efdssdf <efdssdf@gmail.com>
2018-07-13 17:27:04 +0800

    'subscribe'

--- tuitui_weichat/routes/weichat.js
+++ tuitui_weichat/routes/weichat.js
@@ -206,34 +206,34 @@ async function subscribe(openid, config, message, res) {
         var id = JSON.parse(message.EventKey.split('_')[1]).replay;
         QRcodeModel.findById(id,function (err,doc) {
             if(doc){
+                console.log(doc.content,'----------------content')
                 return res.replay(doc.content)
             }else{
                 return res.replay('')
             }
         })
-    }
-
-    var code_list = book_wechat_conf.book_wechat_list;
-    if (config.sub_replay == 0) {
-        if (code_list.indexOf(config.code) == -1) {
-            res.reply('');
-        } else {
-            if (config.replay_text) {
-                res.reply(config.replay_text);
-            } else if (message.Ticket) {
-                bind_user(openid, config.code, message.Ticket, res)
+    }else{
+        var code_list = book_wechat_conf.book_wechat_list;
+        if (config.sub_replay == 0) {
+            if (code_list.indexOf(config.code) == -1) {
+                res.reply('');
             } else {
-                res.reply('省钱助手欢迎您！\r\n一一一一🍒使用攻略一一一一\r\n<搜索优惠>回复：搜索+商品名称\r\n<指定商品优惠查询>请将淘宝商品分享给我！\r\n文字教程：https://w.url.cn/s/ALbRRgu\r\n视频教程：https://w.url.cn/s/ASjqD5J\r\n账户信息请回复：个人信息\r\n邀请好友请回复：邀请好友\r\n订单查询请回复：订单\r\n余额提现请回复：提现\r\n详细教程请回复：帮助\r\n')
+                if (config.replay_text) {
+                    res.reply(config.replay_text);
+                } else if (message.Ticket) {
+                    bind_user(openid, config.code, message.Ticket, res)
+                } else {
+                    res.reply('省钱助手欢迎您！\r\n一一一一🍒使用攻略一一一一\r\n<搜索优惠>回复：搜索+商品名称\r\n<指定商品优惠查询>请将淘宝商品分享给我！\r\n文字教程：https://w.url.cn/s/ALbRRgu\r\n视频教程：https://w.url.cn/s/ASjqD5J\r\n账户信息请回复：个人信息\r\n邀请好友请回复：邀请好友\r\n订单查询请回复：订单\r\n余额提现请回复：提现\r\n详细教程请回复：帮助\r\n')
+                }
+            }
+        } else {
+            var book_id = book_wechat_conf.book_wechat_map[request.params.code];
+            replay_book(book_id, message, res);
+            if (message.Ticket) {
+                getXiaoshuo(message, request.params.code);
             }
         }
-    } else {
-        var book_id = book_wechat_conf.book_wechat_map[request.params.code];
-        replay_book(book_id, message, res);
-        if (message.Ticket) {
-            getXiaoshuo(message, request.params.code);
-        }
     }
-
 }
 
 async function charge_zero(ticket) {
