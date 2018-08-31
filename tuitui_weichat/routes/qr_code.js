d2ab0d234ae8966d4920986055d5b8eb2daedcdf
efdssdf <efdssdf@gmail.com>
2018-08-31 18:35:44 +0800

    'qr'

--- tuitui_weichat/routes/qr_code.js
+++ tuitui_weichat/routes/qr_code.js
@@ -5,7 +5,7 @@ var weichat_util = require('../util/get_weichat_client.js')
 var weichat_conf = require('../conf/weichat.json')
 
 router.get('/show', (req, res, next) => {
-    var codes= [];
+    var codes = [];
     for (var key in weichat_conf) {
         codes.push(weichat_conf[key]);
     }
@@ -26,19 +26,19 @@ router.post('/create', (req, res, next) => {
         code: req.body.code
     }
     var user = new QRcodeModel(qrInfo)
-    user.save(function(err, data) {
-        if(err) {
+    user.save(function (err, data) {
+        if (err) {
             console.log("Error:" + err);
         } else {
             var api = weichat_util.getClient(qrInfo.code);
             var _id = data._id;
-            var str = JSON.stringify({replay: _id})
+            var str = JSON.stringify({replay: _id,tagId:2})
             api.createLimitQRCode(str, (err, result) => {
                 var qrUrl = api.showQRCodeURL(result.ticket) || '';
                 if (qrUrl == '') {
                     console.log(err)
                 } else {
-                    QRcodeModel.findByIdAndUpdate(_id, {qr_code_url: qrUrl}, function(err, data1){
+                    QRcodeModel.findByIdAndUpdate(_id, {qr_code_url: qrUrl}, function (err, data1) {
                         if (err) {
                             console.log("Error:" + err);
                         }
@@ -54,7 +54,7 @@ router.post('/create', (req, res, next) => {
 })
 
 router.get('/get_code', (req, res, next) => {
-    var codes= [];
+    var codes = [];
     for (var key in weichat_conf) {
         codes.push(weichat_conf[key]);
     }
@@ -62,34 +62,17 @@ router.get('/get_code', (req, res, next) => {
 })
 
 router.post('/update', (req, res, next) => {
-    var qrInfo = {
-        name: req.body.name,
-        content: req.body.content,
-        code: req.body.code
-    }
-    QRcodeModel.findByIdAndUpdate(req.body.id, (err, data) => {
-        if (data == '') {
-            res.send({err: '没有数据'})
-        } else {
-            var api = weichat_util.getClient(qrInfo.code);
-            var _id = data._id;
-            var str = JSON.stringify({replay: _id})
-            api.createLimitQRCode(str, (err, result) => {
-                var qrUrl = api.showQRCodeURL(result.ticket) || '';
-                if (qrUrl == '') {
-                    console.log(err)
-                } else {
-                    QRcodeModel.findByIdAndUpdate(_id, {qr_code_url: qrUrl}, function(err, data1){
-                        if (err) {
-                            console.log("Error:" + err);
-                        }
-                        else {
-                            console.log("Res:" + data1);
-                            res.send({data: data1})
-                        }
-                    })
-                }
-            });
+    var name = req.body.name
+    var content = req.body.content
+    var id = req.body.id
+    var str = JSON.stringify({replay: id,tagId:2})
+    QRcodeModel.findByIdAndUpdate(id, {name: name, content: content}, function (err, data) {
+        if (err) {
+            console.log("Error:" + err);
+        }
+        else {
+            console.log("Res:" + data);
+            res.send({data: data})
         }
     })
 })
@@ -100,8 +83,8 @@ router.post('/deleteOne', (req, res, next) => {
             res.send({err: '没有数据'})
         } else {
             QRcodeModel.find({}, (err, result) => {
-                if(result == '') {
-                    res.send({success: '已删除最后一条数据',data: result})
+                if (result == '') {
+                    res.send({success: '已删除最后一条数据', data: result})
                 } else {
                     res.send({success: '删除成功', data: result})
                 }
