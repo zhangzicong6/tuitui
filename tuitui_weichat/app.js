1fdcf476ab75f0f89827a1cd506728ae8483d9c5
weblixin <weblixin@163.com>
2018-09-01 15:01:19 +0800

    ok

--- tuitui_weichat/app.js
+++ tuitui_weichat/app.js
@@ -29,7 +29,7 @@ var tag = require('./routes/tag')
 var app = express();
 
 app.all('*', function(req, res, next) {
-	res.header("Access-Control-Allow-Origin", "http://localhost:8080");
+	res.header("Access-Control-Allow-Origin", "*");
 	res.header('Access-Control-Allow-Credentials', true);
 	res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
 	res.header("Access-Control-Allow-Methods", "PUT,POST,GET,DELETE,OPTIONS");
