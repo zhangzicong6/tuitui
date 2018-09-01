976acb59f56e2dcefa04fb6cf9c90bf418b3ef52
root <lionel@buzzy.cc>
2018-09-01 12:52:24 +0800

    打印关注后的情况

--- tuitui_weichat/routes/weichat.js
+++ tuitui_weichat/routes/weichat.js
@@ -223,7 +223,9 @@ async function subscribe(openid, config, message, res) {
     if (message.EventKey.indexOf("replay") != -1) {
         var id = JSON.parse(message.EventKey.split('_')[1]).replay;
         var tagId = JSON.parse(message.EventKey.split('_')[1]).tagId;
-        UserModel.findOneAndUpdate({"openid":openid},{$addToSet: {tagIds:tagId}},function (data) {
+        console.log('tagId----'+tagId)
+        UserModel.findOneAndUpdate({"openid":openid},{$addToSet: {tagIds:tagId}},{new:true},function (error,data) {
+            console.log(data)
             QRcodeModel.findById(id, function (err, doc) {
                 if (doc) {
                     return res.reply(doc.content)
